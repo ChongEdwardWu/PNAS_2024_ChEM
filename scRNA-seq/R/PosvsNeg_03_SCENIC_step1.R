f8307431a4eaeb8a4e54502da9cf8062c951f291
Chong Wu <44699080+ChongEdwardWu@users.noreply.github.com>
2024-01-31 22:11:47 +0800

    Create PosvsNeg_03_SCENIC_step1.R

---
+++ scRNA-seq/R/PosvsNeg_03_SCENIC_step1.R
@@ -0,0 +1,39 @@
+### Section 0: Preparation --------------------------------------------------
+
+# clear the environment
+rm(list = ls())
+graphics.off()
+gc()
+
+# !set your work diretory
+workdir <- "/your_work_dir/R"
+setwd(workdir)
+
+# Section 1: Load Seurat Obj---------------------------------------------------------------
+# load libraries
+library(Seurat)
+library(tidyverse)
+library(magrittr)
+library(SCENIC)
+library(SingleCellExperiment)
+library(SCopeLoomR)
+set.seed(123)
+
+# !load integrated seurat object
+seu_int <- readRDS("02_WT_Mf_Integration.rds")
+
+# Section 2: Filtering genes for calculating time---------------------------------------------------------------
+exprMat <- seu_int@assays$SCT@data
+cellInfo <- data.frame(seu_int@meta.data)
+dim(cellInfo)
+colnames(cellInfo)
+
+loci1 <- which(rowSums(exprMat) > log1p(1 * .01 * ncol(exprMat)))
+table(rowSums(exprMat) > log1p(1 * .01 * ncol(exprMat)))
+exprMat_filter <- exprMat[loci1, ]
+dim(exprMat_filter)
+
+# Section 3: Tranfer into loom files---------------------------------------------------------------
+loom <- build_loom("seu_int.loom", dgem = exprMat_filter)
+# loom <- add_cell_annotation(loom, cellInfo)
+close_loom(loom)
