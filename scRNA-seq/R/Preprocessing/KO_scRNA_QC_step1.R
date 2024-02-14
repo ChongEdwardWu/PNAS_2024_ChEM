c1caed2c1181d5cda0c40ff41bd35c5b814e7045
Chong Wu <44699080+ChongEdwardWu@users.noreply.github.com>
2024-02-14 10:02:19 +0800

    Add files via upload

---
+++ scRNA-seq/R/Preprocessing/KO_scRNA_QC_step1.R
@@ -0,0 +1,365 @@
+### Section 0: Preparation --------------------------------------------------
+
+# clear the environment
+rm(list = ls())
+graphics.off()
+gc()
+
+suppressMessages(suppressWarnings(source("/ifs1/User/yangbinVM/01.Project/wuchong/.radian_profile")))
+
+# # install packages
+# if (!require("BiocManager", quietly = TRUE)) {
+#   install.packages("BiocManager")
+# }
+
+# BiocManager::install(c(
+#   "DropletUtils", "scater", "tidyverse", "scDblFinder", "BiocParallel",
+#   "Nebulosa", "ggplot2", "remotes", "robustbase", "future"
+# ))
+# install.packages("symphony")
+# remotes::install_github(c("satijalab/seurat", "satijalab/sctransform","satijalab/seurat-wrappers"), ref = "develop")
+# remotes::install_github(c("mojaveazure/seurat-disk", "satijalab/azimuth","ggjlab/scMCA"), ref = "master")
+
+# usethis::create_github_token()
+# the current token is: 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
+# usethis::edit_r_environ()
+
+
+### Section 1-Sepcify sample name and input ------------------------------------------
+
+## ## ## ## ## ## ## ## ## ## ## ##
+##  Reading in 10x Genomics data ##
+## ## ## ## ## ## ## ## ## ## ## ##
+set.seed(123)
+nworkers <- 32
+
+## !input sample name
+sample <- "KO"
+
+# set your work diretory
+workdir <- file.path("/ifs1/User/yangbinVM/01.Project/wuchong/data/scRNAseq-R", sample)
+if (file.exists(workdir)) {
+} else {
+  dir.create(workdir)
+}
+if (file.exists(file.path(workdir,"figures"))) {
+} else {
+  dir.create(file.path(workdir,"figures"))
+}
+setwd(workdir)
+
+# Redirect output and messages to the log file
+current_date <- format(Sys.Date(), "%Y%m%d")
+log_file <- paste0(workdir,"/",sample,"_scRNA_QC_step1_", current_date, ".log")
+output_connection <- file(log_file, open = "wt")
+sink(output_connection)
+sink(output_connection, type = "message")
+
+## !specify the CRcount result here
+suppressMessages(suppressWarnings(library(DropletUtils)))
+suppressMessages(suppressWarnings(library(BiocParallel)))
+sce <- read10xCounts("/ifs1/User/yangbinVM/01.Project/wuchong/data/Peritoneal_ChAT/CRcount/KO/outs/filtered_feature_bc_matrix", col.names = T)
+
+### Section 2-Preprocess before QC ------------------------------------------
+
+## ## ## ## ## ## ## ## ## ## ## ## ##
+##  Cell calling for droplet data   ##
+## ## ## ## ## ## ## ## ## ## ## ## ##
+a <- colSums(counts(sce))
+b <- sort(a)[2] # find the second least UMI count as the lower bound
+# emptyDrops
+e.out <- emptyDrops(counts(sce), lower = b, BPPARAM = MulticoreParam(nworkers))
+## Check cell calling for droplet data
+summary(e.out$FDR <= 0.001)
+
+# plot mito vs detected features
+png(file = "figures/01_Cell_Calling_for_Droplets.png", width = 250, height = 250, units = "mm", res = 150)
+plot(e.out$Total, -e.out$LogProb,
+  col = ifelse(e.out$FDR <= 0.001, "red", "black"),
+  xlab = "Total UMI count", ylab = "-Log Probability"
+)
+dev.off()
+
+# The Limited field in the output indicates whether or not the computed
+# p-value for a particular barcode is bounded by the number of iterations.
+# If any non-significant barcodes are TRUE for Limited,
+# we may need to increase the number of iterations.
+table(Sig = e.out$FDR <= 0.001, Limited = e.out$Limited)
+
+# subset our SingleCellExperiment object to retain only the detected cells
+sce <- sce[,which(e.out$FDR <= 0.001)]
+
+## ## ## ## ## ## ## ## ## ## ## ## ##
+##  Normalization by deconvolution  ##
+## ## ## ## ## ## ## ## ## ## ## ## ##
+
+# We use a pre-clustering step with quickCluster()
+# where cells in each cluster are normalized separately
+# and the size factors are rescaled to be comparable across clusters.
+suppressMessages(suppressWarnings(library(scran)))
+set.seed(123)
+
+clust <- quickCluster(sce, BPPARAM = MulticoreParam(nworkers))
+table(clust)
+deconv.sf <- calculateSumFactors(sce,
+  cluster = clust,
+  BPPARAM = MulticoreParam(nworkers)
+)
+summary(deconv.sf)
+
+# Applying the size factors
+# Scaling and log-transforming
+suppressMessages(suppressWarnings(library(scater)))
+norm <- computeSumFactors(sce, cluster = clust, min.mean = 0.1, BPPARAM = MulticoreParam(nworkers))
+norm <- logNormCounts(norm)
+
+## ## ## ## ## ## ## ## ##
+##   Feature selection  ##
+## ## ## ## ## ## ## ## ##
+
+# Compute the variance of the log-normalized expression values
+dec <- modelGeneVar(norm)
+
+# Visualizing the fit:
+fit <- metadata(dec)
+
+png(file = "figures/02_Feature_selection.png",width = 250, height = 250, units = "mm", res = 150) 
+par(mfrow = c(1, 1))
+plot(fit$mean, fit$var,
+  xlab = "Mean of log-expression",
+  ylab = "Variance of log-expression", main = ""
+)
+curve(fit$trend(x), col = "dodgerblue", add = TRUE, lwd = 2)
+dev.off()
+
+# Selecting highly variable genes
+hvg.var <- getTopHVGs(dec, n = 2000)
+length(hvg.var)
+
+## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
+##   Dimensionality Reduction and Clustering ##
+## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
+
+# PCA
+suppressMessages(suppressWarnings(library(BiocSingular)))
+set.seed(123)
+norm <- fixedPCA(norm,
+  subset.row = hvg.var,
+  BSPARAM = RandomParam(), name = "PCA"
+)
+
+# UMAP
+norm <- runUMAP(norm,
+  dimred = "PCA",
+  BPPARAM = MulticoreParam(nworkers)
+)
+
+# clustering
+suppressMessages(suppressWarnings(library(scran)))
+suppressMessages(suppressWarnings(library(bluster)))
+
+colLabels(norm) <- clusterCells(norm,
+  use.dimred = "PCA",
+  BLUSPARAM = NNGraphParam(type = "jaccard")
+)
+
+# plot the clusters
+suppressMessages(suppressWarnings(library(ggplot2)))
+sceumap <- plotReducedDim(norm, "UMAP", colour_by = "label", text_by = "label")
+ggsave(
+  filename = "figures/03_Dimensionality_Reduction_and_Clustering.png",
+  plot = sceumap,
+  width = 250, height = 250, units = "mm",
+  dpi = 150, device = "png", bg = "white"
+)
+
+## ## ## ## ## ## ## ## ## ## ## ## ##
+## Doublet detection by simulation  ##
+## ## ## ## ## ## ## ## ## ## ## ## ##
+suppressMessages(suppressWarnings(library(scDblFinder)))
+suppressMessages(suppressWarnings(library(scran)))
+set.seed(123)
+
+# Setting up the parameters for consistency with denoisePCA();
+# this can be changed depending on your feature selection scheme.
+dbl <- scDblFinder(
+  norm,
+  clusters = clust,
+  nfeatures = 2000,
+  BPPARAM = MulticoreParam(nworkers)
+)
+table(dbl$scDblFinder.class)
+norm$DoubletScore <- dbl$scDblFinder.score
+norm$DoubletClass <- dbl$scDblFinder.class
+
+# Section 3: Preliminary QC -----------------------------------------------
+suppressMessages(suppressWarnings(library(scater)))
+set.seed(123)
+
+## ## ## ## ## ## ## ## ## ## ## 
+##  Quality Control - Part 2  ##
+#     Mark abnormal 'cells'   ##
+## ## ## ## ## ## ## ## ## ## ## 
+
+# Retrieving the mitochondrial transcripts using genomic locations included in
+# the row-level annotation for the SingleCellExperiment.
+is.mito <- grep("^mt-|^Mtmr|^MT-|MTRNR2L|Mtrnr2l", rowData(norm)$Symbol)
+is.rp <- grep("^Rp[sl]|^RP[SL]", rowData(norm)$Symbol)
+is.hsp <- grep("^HSP|^DNAJ|^Hsp|^Dnaj", rowData(norm)$Symbol)
+is.hemoglobin <- grep("^Hb[ab]-|^HB[^(P)]", rowData(norm)$Symbol)
+is.plat <- grep("Pecam1|Pf4|PECAM1|PF4", rowData(norm)$Symbol)
+
+qc <- perCellQCMetrics(norm, subsets = list(
+  Mito = is.mito, Heatshock = is.hsp, Rp = is.rp,
+  RBC = is.hemoglobin, Plat = is.plat
+))
+colData(norm) <- cbind(colData(norm), qc)
+colnames(colData(norm))
+
+# (1) Filtering with fixed thresholds
+qc.nexprs <- qc$detected < 300
+qc.mito <- qc$subsets_Mito_percent > 10
+qc.rbc <- qc$subsets_RBC_percent > 1
+fix.discard <- qc.nexprs | qc.mito | qc.rbc
+table(fix.discard)
+norm$fix.discard <- fix.discard
+
+# # (2) Alternatively,
+# # We can use methods from robustbase to quantify the "outlyingness" of each
+# # cells based on their QC metrics, and then use isOutlier() to identify
+# # low-quality cells that exhibit unusually high levels of outlyingness.
+# stats <- cbind(
+#   log10(qc$sum),
+#   log10(qc$detected),
+#   qc$subsets_Mito_percent
+# )
+# suppressMessages(suppressWarnings(library(robustbase)))
+# outlying <- adjOutlyingness(stats, only.outlyingness = TRUE)
+# multi.outlier <- isOutlier(outlying, type = "higher", nmads = 5)
+# # attr(multi.outlier, "thresholds")
+# table(multi.outlier)
+# norm$adapt.discard <- multi.outlier
+
+# table(cbind(fix.discard, multi.outlier))
+
+# plot mito vs detected features
+png(file = "figures/04_Cell_Filtering.png",width = 250, height = 250, units = "mm", res = 150) 
+plot(qc$detected, qc$subsets_Mito_percent,
+  log = "x",
+  xlab = "Detected features", ylab = "Mitochondrial %"
+)
+points(qc$detected[fix.discard], qc$subsets_Mito_percent[fix.discard],
+  col = "dodgerblue", pch = 16, cex = 0.5
+)
+# points(qc$sum[multi.outlier], qc$subsets_Mito_percent[multi.outlier],
+#   col = "red", pch = 16, cex = 0.5
+# )
+legend("topright",
+  legend = c("Fix", "Adaptive"),
+  col = c("dodgerblue", "red"), lty = 2, cex = 1
+)
+dev.off()
+
+discard <- fix.discard
+# discard <- fix.discard | multi.outlier
+# summary(cbind(fix.discard, multi.outlier, discard))
+norm$discard <- discard
+
+# Check diagnostic plots
+diagnostic <- gridExtra::grid.arrange(
+  plotColData(norm, x = "DoubletClass", y = "sum", colour_by = "DoubletClass") +
+    ggtitle("Doublets"),
+  plotColData(norm, x = "discard", y = "sum", colour_by = "discard") +
+    ggtitle("Total count"),
+  plotColData(norm,
+    x = "discard", y = "subsets_Mito_percent",
+    colour_by = "discard"
+  ) + ggtitle("Mito percent"),
+  plotColData(norm,
+    x = "discard", y = "subsets_Rp_percent",
+    colour_by = "discard"
+  ) + ggtitle("ribosomal protein percent"),
+  plotColData(norm,
+    x = "discard", y = "subsets_Heatshock_percent",
+    colour_by = "discard"
+  ) + ggtitle("Heatshock protein percent"),
+  plotColData(norm, x = "sum", y = "detected", colour_by = "discard") +
+    ggtitle("Detected features"),
+    plotColData(norm,
+    x = "sum", y = "subsets_RBC_percent",
+    colour_by = "discard"
+  ) + ggtitle("hemoglobin percent"),
+  plotColData(norm,
+    x = "sum", y = "subsets_Plat_detected",
+    colour_by = "discard"
+  ) + ggtitle("platelet percent"),
+  ncol = 3
+)
+ggsave(
+  filename = "figures/05_Diagnostic_Plots.png",
+  plot = diagnostic,
+  width = 500, height = 250, units = "mm",
+  dpi = 150, device = "png", bg = "white"
+)
+
+# Check cluster quality
+clusQC <- gridExtra::grid.arrange(
+  plotColData(norm, x = "label", y = "DoubletScore", colour_by = "DoubletClass") +
+    ggtitle("Doublets"),
+  plotColData(norm, x = "label", y = "sum", colour_by = "label") +
+    ggtitle("Total count"),
+  plotColData(norm, x = "label", y = "detected", colour_by = "label") +
+    ggtitle("Detected features"),
+  plotColData(norm, x = "label", y = "subsets_Mito_percent", colour_by = "label") +
+    ggtitle("Mito percent"),
+  plotColData(norm, x = "label", y = "subsets_Rp_percent", colour_by = "label") +
+    ggtitle("ribosomal genes percent"),
+  plotColData(norm, x = "label", y = "subsets_Heatshock_percent", colour_by = "label") +
+    ggtitle("heat shock protein genes percent"),
+  plotColData(norm, x = "label", y = "subsets_RBC_percent", colour_by = "label") +
+    ggtitle("RBC percent"),
+  plotColData(norm, x = "label", y = "subsets_Plat_percent", colour_by = "label") +
+    ggtitle("platelets percent"),
+  plotColData(norm, x = "label", y = "discard", colour_by = "label") +
+    ggtitle("Discard"),
+  ncol = 3
+)
+ggsave(
+  filename = "figures/06_Cluster_QC_Plots.png",
+  plot = clusQC,
+  width = 500, height = 500, units = "mm",
+  dpi = 150, device = "png", bg = "white"
+)
+
+# see preliminary filtering results in clusters
+sceumap <- gridExtra::grid.arrange(
+  plotReducedDim(norm[, norm$discard == 0], "UMAP", colour_by = "label", text_by = "label") +
+    ggtitle("Cells remained"),
+  plotReducedDim(norm[, norm$discard == 1], "UMAP", colour_by = "label", text_by = "label") +
+    ggtitle("Cells discarded"),
+  ncol = 2
+)
+ggsave(
+  filename = "figures/07_PreFiltering_UMAP.png",
+  plot = sceumap,
+  width = 250, height = 250, units = "mm",
+  dpi = 150, device = "png", bg = "white"
+)
+
+
+## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
+# pause here
+setwd(workdir)
+saveRDS(norm, file = paste0("01_QC_step1_",sample,".rds"))
+print(paste0("Job ",sample," has been sucessfully done at ", Sys.time(), "."))
+## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
+sink()
+sink(type = "message")
+rm(list = ls())
+graphics.off()
+gc()
+
+# norm <- norm[, norm$DoubletClass == "singlet"]
+
+# !Run in server
+nohup Rscript --vanilla /ifs1/User/yangbinVM/01.Project/wuchong/data/scRNAseq-R/KO/KO_scRNA_QC_step1.R &
---
+++ scRNA-seq/R/Preprocessing/Neg_scRNA_QC_step2.R
@@ -0,0 +1,293 @@
+### Section 0: Preparation --------------------------------------------------
+# clear the environment
+rm(list = ls())
+graphics.off()
+gc()
+
+suppressMessages(suppressWarnings(source("/ifs1/User/yangbinVM/01.Project/wuchong/.radian_profile")))
+
+# # install packages
+# if (!require("BiocManager", quietly = TRUE)) {
+#   install.packages("BiocManager")
+# }
+
+# BiocManager::install(c(
+#   "DropletUtils", "scater", "tidyverse", "scDblFinder", "BiocParallel",
+#   "Nebulosa", "ggplot2", "remotes", "robustbase", "future","clustifyr", "clustifyrdatahub"
+# ))
+
+# remotes::install_github(c("satijalab/seurat", "satijalab/sctransform","satijalab/seurat-wrappers"), ref = "develop")
+# remotes::install_github(c("mojaveazure/seurat-disk", "satijalab/azimuth","ggjlab/scMCA"), ref = "master")
+
+# usethis::create_github_token()
+# the current token is: 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
+# usethis::edit_r_environ()
+
+
+### Section 1-identify unqualified clusters ------------------------------------------
+suppressMessages(suppressWarnings(library(scater)))
+
+set.seed(123)
+nworkers <- 16
+
+## ! type sample name
+sample <- "Neg"
+## ! type sample name
+species <- "mm"
+# species <- "hs"
+
+## ! path to cellranger outs
+cr_path <- file.path("/ifs1/User/yangbinVM/01.Project/wuchong/data/Peritoneal_ChAT/CRcount/Neg/outs/filtered_feature_bc_matrix")
+
+## ! path to loom
+loom_path <- file.path("/ifs1/User/yangbinVM/01.Project/wuchong/data/Peritoneal_ChAT/CRcount/Neg/velocyto/Neg.loom")
+
+
+# set your work diretory
+workdir <- file.path("/ifs1/User/yangbinVM/01.Project/wuchong/data/scRNAseq-R", sample)
+setwd(workdir)
+
+# load preprocessed data
+norm <- readRDS(file = paste0("01_QC_step1_", sample, ".rds"))
+
+# !Summary of filter diagnosis - Discarded clusters
+colData(norm)$discard.cl <- ifelse(norm$label %in% c(
+  #8,21,25,27,29,30,32,34,35,# , # doublets
+  11,14,15,18,19,20 # bad cells
+   # RBCs and platelets
+),
+1, 0
+)
+
+# !adjust threholds
+qc.nexprs <- norm$detected < 1000
+qc.mito <- norm$subsets_Mito_percent > 10
+qc.rbc <- norm$subsets_RBC_percent > 1
+qc.doublet <- norm$DoubletClass == "doublet"
+qc.cluster <- norm$discard.cl == 1
+discard.new <- qc.nexprs | qc.mito | qc.rbc | qc.doublet | qc.cluster
+
+
+# Redirect output and messages to the log file
+current_date <- format(Sys.Date(), "%Y%m%d")
+log_file <- paste0(workdir,"/",sample,"_scRNA_QC_step2_", current_date, ".log")
+output_connection <- file(log_file, open = "wt")
+sink(output_connection)
+sink(output_connection, type = "message")
+Sys.time()
+
+# summary of filtering
+print("Summary of filtering")
+table(discard.new)
+
+norm$discard <- discard.new
+
+# see final filtering results in clusters
+sceumap <- gridExtra::grid.arrange(
+  plotReducedDim(norm[, norm$discard == 0], "UMAP", colour_by = "label", text_by = "label") +
+    ggtitle("Cells remained"),
+  plotReducedDim(norm[, norm$discard == 1], "UMAP", colour_by = "label", text_by = "label") +
+    ggtitle("Cells discarded"),
+  ncol = 2
+)
+ggsave(
+  filename = "figures/08_Final_Filtering_UMAP.png",
+  plot = sceumap,
+  width = 250, height = 250, units = "mm",
+  dpi = 150, device = "png", bg = "white"
+)
+
+# We can diagnose cell type loss by looking for systematic
+# differences in gene expression between the discarded and retained cells.
+rownames(rowData(norm)) <- rowData(norm)$Symbol
+lost <- calculateAverage(counts(norm)[, norm$discard])
+kept <- calculateAverage(counts(norm)[, !norm$discard])
+suppressMessages(suppressWarnings(library(edgeR)))
+logged <- cpm(cbind(lost, kept), log = TRUE, prior.count = 2)
+logFC <- logged[, 1] - logged[, 2]
+abundance <- rowMeans(logged)
+png(file = "figures/09_filtered_cells_DEGs.png", width = 250, height = 250, units = "mm", res = 150)
+plot(abundance, logFC, xlab = "Average count", ylab = "Log-FC (lost/kept)", pch = 16)
+points(abundance[discard.new], logFC[discard.new], col = "dodgerblue", pch = 16)
+dev.off()
+
+# The presence of a distinct population in the discarded pool
+# as a set of genes that are strongly upregulated in lost
+# Let's see what these genes are
+DEGs <- as.data.frame(lost[logFC > 1])
+# View(DEGs)
+print("DEGs between cells remained and discarded")
+rowData(norm)$Symbol[which(rowData(norm)$ID %in% rownames(DEGs))]
+
+
+### Section 2-final cell filtering decision ------------------------------------------
+
+## ## ## ## ## ## ## ## ## ## ## ## #
+##  Final Cell Filtering Decision ##
+## ## ## ## ## ## ## ## ## ## ## ## #
+suppressMessages(suppressWarnings(library(scran)))
+suppressMessages(suppressWarnings(library(BiocParallel)))
+
+# Re-subset our SingleCellExperiment object to retain only the detected cells
+set.seed(123)
+
+# Filter cells
+sce <- norm[, (!norm$discard)]
+
+# Assign sample
+sce$group <- sample
+
+# Assign Cell-Cycle Scores
+if (species == "mm") {
+  CCpairs <- readRDS(system.file("exdata", "mouse_cycle_markers.rds", package = "scran"))
+} else if(species == "hs") {
+  CCpairs <- readRDS(system.file("exdata", "human_cycle_markers.rds", package = "scran"))
+}
+
+assignments <- cyclone(sce, CCpairs,
+  gene.names = rowData(sce)$ID,
+  BPPARAM = MulticoreParam(nworkers)
+)
+sce$G1.score <- assignments$normalized.scores$G1
+sce$S.score <- assignments$normalized.scores$S
+sce$G2M.score <- assignments$normalized.scores$G2M
+sce$CCphase <- assignments$phases
+
+# calculate difference between the G2M and S phase scores
+sce$CC.Diff <- sce$S.score - sce$G2M.score
+# Add integrated cell IDs
+library(stringr)
+sce$CellID <- str_c(paste0(sample, ":"), str_sub(string = colnames(sce), start = 1, end = 16), "x")
+colnames(sce) <- sce$CellID
+
+## Add cell type annotation -- For mouse only
+if (species == "mm") {
+  suppressMessages(suppressWarnings(library(scMCA)))
+  rownames(sce) <- rowData(sce)$Symbol
+  mca_result <- scMCA(
+    scdata = assays(sce)@listData[["logcounts"]],
+    numbers_plot = 3
+  )
+  # scMCA_vis(mca_result)
+  # View(mca_result[["scMCA_probility"]])
+  sce$CellType_mca <- mca_result[["scMCA"]]
+
+  # perform annotation by calling SingleR()
+  suppressMessages(suppressWarnings(library(SingleR)))
+  load("/ifs1/User/yangbinVM/01.Project/wuchong/Ref_genome/CellType_ref/Immgen_ref/ImmGen_reference-Heng_2008.RData")
+  pred <- SingleR(
+    test = sce,
+    ref = immgen,
+    labels = immgen$label.fine,
+    assay.type.test = 1,
+    BPPARAM = MulticoreParam(nworkers)
+  )
+  sce$CellType_immgen <- mca_result[["scMCA"]] <- pred$pruned.labels
+}
+
+## export colDatas
+colData <- as_tibble(sce@colData)
+
+# save colData
+saveRDS(
+  colData,
+  file = paste0(current_date, "_", sample, "_colData.rds")
+)
+# colData <- readRDS(paste0(current_date,"_",sample,"_colData.rds"))
+
+# Section 3: construct Seurat object ----------------------------------------------
+## Using Seurat to integrate datasets ##
+suppressMessages(suppressWarnings(library(Seurat)))
+suppressMessages(suppressWarnings(library(SeuratDisk)))
+suppressMessages(suppressWarnings(library(SeuratWrappers)))
+suppressMessages(suppressWarnings(library(patchwork)))
+suppressMessages(suppressWarnings(library(ggplot2)))
+
+library(future)
+# plan('sequential')
+plan('multicore', workers = nworkers)
+
+set.seed(123)
+
+# read in 10x cellranger outcomes
+counts <- Read10X(data.dir = cr_path)
+seu <- CreateSeuratObject(counts = counts)
+
+# rename cells in accordance with the loom file
+newnames <- str_c(paste0(sample, ":"), str_sub(string = Cells(seu), start = 1, end = 16), "x")
+seu <- RenameCells(seu, new.names = newnames)
+print("Check whether the Cell IDs are properly formatted in the created seurat object")
+all(Cells(seu) %in% colData$CellID)
+# filter cells
+seu <- seu[, Cells(seu) %in% colData$CellID]
+
+# read in loom file
+if (file.exists(loom_path)) {
+  loom <- ReadVelocity(file = loom_path)
+  # convert loom to seurat object
+  seu_loom <- as.Seurat(loom)
+  seu_loom <- subset(seu_loom, cells= Cells(seu))
+  # create spliced and unsliced assays
+  spliced_assay <- CreateAssayObject(counts = seu_loom@assays$spliced$counts[, Cells(seu)])
+  unspliced_assay <- CreateAssayObject(counts = seu_loom@assays$unspliced$counts[, Cells(seu)])
+  print("Check whether the Cell IDs are the same in the seurat object and in the newly created assays")
+  all(Cells(seu) == colnames(spliced_assay), Cells(seu) == colnames(unspliced_assay))
+  seu[["spliced"]] <- spliced_assay
+  seu[["unspliced"]] <- unspliced_assay
+}
+
+# import the colData from the above SingleCellExperiment subject
+seu$CellID <- Cells(seu)
+seu@meta.data <- left_join(
+  as_tibble(seu@meta.data),
+  colData,
+  by = "CellID"
+) %>%
+  column_to_rownames("CellID")
+
+## Perform normalization and dimensionality reduction
+# perform normalization using SCTransform
+# with an additional flag vst.flavor="v2" to invoke the v2 regularization
+vars_to_regress <- c("subsets_Mito_percent", "subsets_Rp_percent", "S.score", "G2M.score") # total.UMI
+
+
+seu <- SCTransform(seu,
+  vst.flavor = "v2",
+  verbose = T,
+  return.only.var.genes = F,
+  # We suggest regressing out the difference between the G2M and S phase scores.
+  # This means that signals separating non-cycling cells and cycling cells will be maintained, but differences in cell cycle phase among proliferating cells (which are often uninteresting), will be regressed out of the data
+  vars.to.regress = vars_to_regress
+) %>% RunPCA(npcs = 30, verbose = T)
+
+
+# Cell type annotation - Azimuth
+library(Azimuth)
+options(timeout=10000)
+if (species == "hs") {
+  # Map data to Azimuth annotation
+  # The RunAzimuth function can take a Seurat object as input
+  seu.annot <- RunAzimuth(
+    seu,
+    reference = "/ifs1/User/yangbinVM/01.Project/wuchong/Ref_genome/CellType_ref/Azimuth_ref/Human_PBMC"
+  )
+  # transfer the annotation results (which is in the metadata)
+  seu@meta.data <- seu.annot@meta.data
+}
+
+# save result and print sucess information
+saveRDS(
+  seu,
+  file = paste0(sample, "_seu.rds")
+)
+print(paste0("Job ",sample," has been sucessfully done at ", Sys.time(), "."))
+
+sink()
+sink(type = "message")
+
+rm(list = ls())
+graphics.off()
+gc()
+
+# Run in server
+# nohup Rscript --vanilla /ifs1/User/yangbinVM/01.Project/wuchong/data/scRNAseq-R/Neg/Neg_scRNA_QC_step2.R &
